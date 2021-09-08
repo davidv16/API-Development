7022fc6e7137b02b130e857b71514ddcb7dce9ca
davidv16 <dabbithekking@gmail.com>
2021-09-08 21:56:21 +0000

    sp1 done

--- Small assignment II - Exterminator/Exterminator.WebApi/Startup.cs
+++ Small assignment II - Exterminator/Exterminator.WebApi/Startup.cs
@@ -3,41 +3,53 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Exterminator.Services.Implementations;
+using Exterminator.Services.Interfaces;
+using Exterminator.Repositories.Data;
+using Exterminator.Repositories.Implementations;
+using Exterminator.Repositories.Interfaces;
+using Exterminator.WebApi.Extensions;
 
 namespace Exterminator.WebApi
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-
-            // TODO: Register dependencies
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            // TODO: Setup global exception handling
-
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+      // TODO: Register dependencies
+      services.AddTransient<ILogService, LogService>();
+      services.AddTransient<IGhostbusterService, GhostbusterService>();
+      services.AddTransient<ILogRepository, LogRepository>();
+      services.AddTransient<IGhostbusterRepository, GhostbusterRepository>();
+      services.AddTransient<IGhostbusterDbContext, GhostbusterDbContext>();
+
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      // TODO: Setup global exception handling
+
+      app.UseRouting();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
