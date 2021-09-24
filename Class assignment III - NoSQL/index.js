47ab42ab96ddb4ce0efa72f17d354825ddc61422
Einar Sigurðsson <einars@fileice.org>
2021-09-24 18:05:23 +0000

    FIX: 1.10

--- Class assignment III - NoSQL/index.js
+++ Class assignment III - NoSQL/index.js
@@ -3,36 +3,35 @@ const { TIGER_SHARK, HAMMERHEAD_SHARK, GREAT_WHITE_SHARK, BULL_SHARK } = require
 
 ;(async () => {
   // 1.1. Get all sharks
-  console.log('1.1. Get all sharks')
   await Shark.find({}, (err, sharks) => {
     if (err) { throw new Error(err) }
+    console.log('1.1. Get all sharks')
     console.log(sharks)
   })
-
+  
   // 1.2. Get all tiger sharks
-  console.log('1.2. Get all tiger sharks')
   await Shark.find({ species: TIGER_SHARK }, (err, sharks) => {
     if (err) { throw new Error(err) }
+    console.log('1.2. Get all tiger sharks')
     console.log(sharks)
   })
-
+  
   // 1.3. Get all tiger and bull sharks
-  console.log('1.3. Get all tiger and bull sharks')
   var filter = [TIGER_SHARK, BULL_SHARK]
   await Shark.find({ species: { $in: filter } }, (err, sharks) => {
     if (err) { throw new Error(err) }
+    console.log('1.3. Get all tiger and bull sharks')
     console.log(sharks)
   })
-
+  
   // 1.4. Get all sharks except great white sharks
-  console.log('1.4. Get all sharks except great white sharks')
   await Shark.find({ species: { $ne: GREAT_WHITE_SHARK } }, (err, sharks) => {
     if (err) { throw new Error(err) }
+    console.log('1.4. Get all sharks except great white sharks')
     console.log(sharks)
   })
-
+  
   // 1.5. Get all sharks that have been known to attack
-  console.log('1.5. Get all sharks that have been known to attack')
   await Shark.aggregate([
     {
       $lookup: {
@@ -56,11 +55,11 @@ const { TIGER_SHARK, HAMMERHEAD_SHARK, GREAT_WHITE_SHARK, BULL_SHARK } = require
   ], (err, sharks) => {
     if (err) { throw new Error(err) }
     //console.log(sharks.filter(shark => shark.attacks.length > 0))
+    console.log('1.5. Get all sharks that have been known to attack')
     console.log(sharks)
   })
-
+  
   // 1.6. Get all areas with registered attacks
-  console.log('1.6. Get all areas with registered attacks')
   await Area.aggregate([
     {
       $lookup: {
@@ -84,11 +83,11 @@ const { TIGER_SHARK, HAMMERHEAD_SHARK, GREAT_WHITE_SHARK, BULL_SHARK } = require
   ], (err, sharks) => {
     if (err) { throw new Error(err) }
     //console.log(sharks.filter(shark => shark.attacks.length > 0))
+    console.log('1.6. Get all areas with registered attacks')
     console.log(sharks)
   })
-
+  
   // 1.7. Get all areas with more than 5 registered attacks
-  console.log('1.7. Get all areas with more than 5 registered attacks')
   await Area.aggregate([
     {
       $lookup: {
@@ -121,11 +120,11 @@ const { TIGER_SHARK, HAMMERHEAD_SHARK, GREAT_WHITE_SHARK, BULL_SHARK } = require
   ], (err, sharks) => {
     if (err) { throw new Error(err) }
     //console.log(sharks.filter(shark => shark.attacks.length > 0))
+    console.log('1.7. Get all areas with more than 5 registered attacks')
     console.log(sharks)
   })
-
+  
   // 1.8. Get the area with the most registered shark attacks
-  console.log('1.8. Get the area with the most registered shark attacks')
   await Area.aggregate([
     {
       $lookup: {
@@ -158,12 +157,12 @@ const { TIGER_SHARK, HAMMERHEAD_SHARK, GREAT_WHITE_SHARK, BULL_SHARK } = require
     }
   ], (err, sharks) => {
     if (err) { throw new Error(err) }
+    console.log('1.8. Get the area with the most registered shark attacks')
     //console.log(sharks.filter(shark => shark.attacks.length > 0))
     console.log(sharks)
   })
-
+  
   // 1.9. Get the total count of great white shark attacks
-  console.log('1.9. Get the total count of great white shark attacks')
   await Shark.aggregate([
     {
       $match: { species: { $eq: GREAT_WHITE_SHARK } }
@@ -188,14 +187,19 @@ const { TIGER_SHARK, HAMMERHEAD_SHARK, GREAT_WHITE_SHARK, BULL_SHARK } = require
         count: { $sum: 1 }
       }
     },
+    {
+      $project: {
+        _id: 0
+      }
+    }
   ], (err, sharks) => {
     if (err) { throw new Error(err) }
+    console.log('1.9. Get the total count of great white shark attacks')
     //console.log(sharks.filter(shark => shark.attacks.length > 0))
     console.log(sharks)
   })
-
+  
   // 1.10. Get the total count of hammerhead and tiger shark attacks
-  console.log('1.10. Get the total count of hammerhead and tiger shark attacks')
   await Shark.aggregate([
     {
       $match: { species: { $in: [HAMMERHEAD_SHARK, TIGER_SHARK] } }
@@ -216,12 +220,16 @@ const { TIGER_SHARK, HAMMERHEAD_SHARK, GREAT_WHITE_SHARK, BULL_SHARK } = require
     },
     {
       $group: {
-        _id: '$species',
+        _id: null,
         count: { $sum: 1 }
       }
     },
+    {
+      $project: { _id: 0 }
+    }
   ], (err, sharks) => {
     if (err) { throw new Error(err) }
+    console.log('1.10. Get the total count of hammerhead and tiger shark attacks')
     //console.log(sharks.filter(shark => shark.attacks.length > 0))
     console.log(sharks)
   })
