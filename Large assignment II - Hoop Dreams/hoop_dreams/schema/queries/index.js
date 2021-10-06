8fb11a03465ecb61f5ac4d88487d3aad1188c5b2
Einar Sigurðsson <einarsi@landspitali.is>
2021-10-06 22:45:10 +0000

    WIP: schema queries

--- Large assignment II - Hoop Dreams/hoop_dreams/schema/queries/index.js
+++ Large assignment II - Hoop Dreams/hoop_dreams/schema/queries/index.js
@@ -1,2 +1,10 @@
 module.exports = `
+  type Query {
+    allBasketballFields: [BasketballField!]!
+    allPickupGames: [PickupGame!]!
+    allPlayers: [Player!]!
+    basketballField: BasketballField!
+    pickupGame: PickupGame!
+    player: Player!
+  }
 `
