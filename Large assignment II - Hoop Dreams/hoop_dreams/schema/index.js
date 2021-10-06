120fff4e5d9b3c214f83a1bb3ea2ed50addb3f43
Einar Sigurðsson <einarsi@landspitali.is>
2021-10-06 21:59:00 +0000

    WIP: setting up schema

---
+++ Large assignment II - Hoop Dreams/hoop_dreams/schema/index.js
@@ -0,0 +1,15 @@
+const inputs = require('./inputs')
+const mutations = require('./mutations')
+const queries = require('./queries')
+const scalar = require('./scalar')
+const types = require('./types')
+const enums = require('./enums')
+
+module.exports = `
+  ${inputs}
+  ${mutations}
+  ${queries}
+  ${scalar}
+  ${types}
+  ${enums}
+`
