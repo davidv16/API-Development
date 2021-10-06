75a9f67409d1a01328a91ea8b3b8fb4842142df1
Einar Sigurðsson <einarsi@landspitali.is>
2021-10-06 21:59:35 +0000

    CLEANUP: made code compliant with StandardJS

--- Large assignment II - Hoop Dreams/hoop_dreams/index.js
+++ Large assignment II - Hoop Dreams/hoop_dreams/index.js
@@ -1,11 +1,11 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server')
+const typeDefs = '' // require('./schema')
+const resolvers = {} // require('./resolvers')
 
 const server = new ApolloServer({
-    /*
-        Add typeDefs
-        Add resolvers
-    */
-});
+  typeDefs,
+  resolvers
+})
 
 server.listen()
-    .then(({ url }) => console.log(`GraphQL Service is running on ${ url }`));
+  .then(({ url }) => console.log(`GraphQL Service is running on ${url}`))
