ea7785cfdac48a8f412361303f5e1df21950ce32
Einar Sigurðsson <einarsi@landspitali.is>
2021-10-07 22:04:38 +0000

    UPDATE: added temp pickup games to service

--- Large assignment II - Hoop Dreams/hoop_dreams/resolvers/basketballFieldResolver.js
+++ Large assignment II - Hoop Dreams/hoop_dreams/resolvers/basketballFieldResolver.js
@@ -2,7 +2,31 @@
 
 module.exports = {
   queries: {
-    allBasketballFields: (parent, args, context) => context.services.basketballFieldService.getAllBasketballFields(),
-    basketballField: (parent, args, context) => context.services.basketballFieldService.getBasketballField(args.id)
+    allBasketballFields: async (parent, args, context) => {
+      const service = context.services.basketballFieldService
+      const basketballFields = await service.getAllBasketballFields()
+
+      console.log(basketballFields)
+
+      for (const field of basketballFields) {
+        // TODO: add pickup games to field
+        field.pickupGames = [
+          { id: 'asdf' }
+        ]
+      }
+
+      return basketballFields
+    },
+    basketballField: async (parent, args, context) => {
+      const service = context.services.basketballFieldService
+      const basketballField = await service.getBasketballField(args.id)
+
+      // TODO: add pickup games to basketball field
+      basketballField.pickupGames = [
+        { id: 'asdf' }
+      ]
+
+      return basketballField
+    }
   }
 }
