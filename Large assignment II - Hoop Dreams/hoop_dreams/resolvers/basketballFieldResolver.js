47f04bd43ef56c66bc1df0923130b7bac3c6d92f
Einar Sigurðsson <einarsi@landspitali.is>
2021-10-07 20:08:29 +0000

    CLEANUP: removing unused code

--- Large assignment II - Hoop Dreams/hoop_dreams/resolvers/basketballFieldResolver.js
+++ Large assignment II - Hoop Dreams/hoop_dreams/resolvers/basketballFieldResolver.js
@@ -3,10 +3,6 @@ const basketballFieldService = require('../services/basketballFieldService')
 module.exports = {
   queries: {
     allBasketballFields: () => basketballFieldService.getAllBasketballFields(),
-    basketballField: (parent, args, context) => {
-      console.log(args)
-
-      return basketballFieldService.getBasketballField(args.id)
-    }
+    basketballField: (parent, args, context) => basketballFieldService.getBasketballField(args.id)
   }
 }
