319038e88c1366f3bb5ecd4c8ecf23a7b2034cd9
Einar Sigurðsson <einarsi@landspitali.is>
2021-10-07 22:16:25 +0000

    ADD: mongodb with example db file

--- Large assignment II - Hoop Dreams/hoop_dreams/index.js
+++ Large assignment II - Hoop Dreams/hoop_dreams/index.js
@@ -1,12 +1,14 @@
 const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
+const database = require('./data/db')
 const serviceCollection = require('./services')
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: {
+    db: database,
     services: serviceCollection
   }
 })
