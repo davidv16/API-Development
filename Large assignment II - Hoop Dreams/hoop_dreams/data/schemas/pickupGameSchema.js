23a52e3be228c382e034c77490d48a55643b7c8c
Einar Sigurðsson <einars@fileice.org>
2021-10-10 01:11:29 +0000

    UPDATE: database schema and seed

--- Large assignment II - Hoop Dreams/hoop_dreams/data/schemas/pickupGameSchema.js
+++ Large assignment II - Hoop Dreams/hoop_dreams/data/schemas/pickupGameSchema.js
@@ -4,5 +4,5 @@ module.exports = new Schema({
   start: { type: Date, required: true },
   end: { type: Date, required: true },
   locationId: { type: String, required: true },
-  host: { type: String, required: true }
+  hostId: { type: String, required: true }
 })
--- Large assignment II - Hoop Dreams/hoop_dreams/data/seed.js
+++ Large assignment II - Hoop Dreams/hoop_dreams/data/seed.js
@@ -1,89 +1,81 @@
-//const { Area, Shark, Attack, connection } = require('./db');
-const { Player, PickupGame, connection } = require('./db');
-//const { TIGER_SHARK, HAMMERHEAD_SHARK, GREAT_WHITE_SHARK, BULL_SHARK, WHALE_SHARK, NORTH_AMERICA, AUSTRALIA, EUROPE, MEXICO_BEACH, MALIBU_BEACH, GOLD_COAST, BONDI_BEACH, REYKJANES_BEACH } = require('../constants');
-const cliProgress = require('cli-progress');
+const { Player, PickupGame, PickupGamePlayers, connection } = require('./db')
+const cliProgress = require('cli-progress')
 
-//const getResourceIdByName = (resources, prop, value) => resources.find(elem => elem[prop] === value);
-const bar = new cliProgress.Bar({}, cliProgress.Presets.rect);
-bar.start(100, 0);
+const bar = new cliProgress.Bar({}, cliProgress.Presets.rect)
+bar.start(100, 0)
 
 // Drop all collections before execution
 Object.keys(connection.collections).forEach(collection => {
-    if (collection === 'PickupGame') { PickupGame.collection.drop(); }
-    if (collection === 'Player') { Player.collection.drop(); }
-});
+  if (collection === 'PickupGamePlayers') { PickupGamePlayers.collection.drop() }
+  if (collection === 'PickupGame') { PickupGame.collection.drop() }
+  if (collection === 'Player') { Player.collection.drop() }
+})
 
-// CHRISTMAS TREE!!
-//           *
-//          /.\
-//         /..'\
-//         /'.'\
-//        /.''.'\
-//        /.'.'.\
-// "'""""/'.''.'.\""'"'"
-//       ^^^[_]^^^
-PickupGame.insertMany([
-    { 
-        start: Date.now(),
-        end: Date.now(),
-        locationId: "basketballfield",
-        registeredPlayerIds: ["player1", "player2"],
-        host: "player"
-     
+// Create players
+Player.insertMany([
+  { name: 'Einar' },
+  { name: 'Davíð' },
+  { name: 'Michael Jordan' },
+  { name: 'Magic Johnson' },
+  { name: 'Shaquille O\'Neal' },
+  { name: 'Charles Barkley' }
+], (err, players) => {
+  if (err) { throw new Error(err) }
+
+  console.log(players)
+  bar.update(33);
+  // Create pickup games
+  PickupGame.insertMany([
+    {
+      start: new Date('2021-01-01'),
+      end: new Date('2021-01-02'),
+      locationId: 'ef42039e-77bc-40a3-8121-c2a5424ebcdb',
+      hostId: players[0]._id.toString()
     },
-    { 
-        start: Date.now(),
-        end: Date.now(),
-        locationId: "basketballfield1",
-        registeredPlayerIds: ["player2", "player4"],
-        host: "player1"
-     
-    },
-    { 
-        start: Date.now(),
-        end: Date.now(),
-        locationId: "basketballfield2",
-        registeredPlayerIds: ["player3", "player4"],
-        host: "player2"
-     
-    },
-    { 
-        start: Date.now(),
-        end: Date.now(),
-        locationId: "basketballfield3",
-        registeredPlayerIds: ["player11", "player21"],
-        host: "player3"
-     
+    {
+      start: new Date('2021-01-02'),
+      end: new Date('2021-01-03'),
+      locationId: 'ef42039e-77bc-40a3-8121-c2a5424ebcdb',
+      hostId: players[1]._id.toString()
     }
-    
-], err => {
-    if (err) { throw new Error(err); }
-    bar.update(50);
-    Player.insertMany([
-            {
-                name: "magic johnson",
-                playedGames: ["game 1", "game 2"]
-            },
-            {
-                name: "michael jordan",
-                playedGames: ["game 3", "game 4"]
-            },
-            {
-                name: "shaquille oneal",
-                playedGames: ["game 5", "game 6"]
-            },
-            {
-                name: "hinn gaurinn",
-                playedGames: ["game 7", "game 8"]
-            }
-        ], err => {
-            if (err) { throw new Error(err); }
-            bar.update(100);
-    
-            bar.stop();
-            connection.close();
-        
-        });
+  ], (err, games) => {
+    if (err) { throw new Error(err) }
 
-});
-    
+    console.log(games)
+    bar.update(66);
+    // Assign players to pickup games
+    PickupGamePlayers.insertMany([
+      {
+        pickupGameId: games[0]._id.toString(),
+        playerId: players[0]._id.toString()
+      },
+      {
+        pickupGameId: games[0]._id.toString(),
+        playerId: players[2]._id.toString()
+      },
+      {
+        pickupGameId: games[0]._id.toString(),
+        playerId: players[3]._id.toString()
+      },
+      {
+        pickupGameId: games[1]._id.toString(),
+        playerId: players[1]._id.toString()
+      },
+      {
+        pickupGameId: games[1]._id.toString(),
+        playerId: players[4]._id.toString()
+      },
+      {
+        pickupGameId: games[1]._id.toString(),
+        playerId: players[5]._id.toString()
+      },
+    ], (err, gamePlayers) => {
+      if (err) { throw new Error(err) }
+
+      console.log(gamePlayers)
+      bar.update(100);
+      bar.stop()
+      connection.close()
+    })
+  })
+})
