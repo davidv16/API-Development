8ff2aab7d6483ec8cdccbabd2adcc0f5e864ad5c
davidv16 <dabbithekking@gmail.com>
2021-10-10 00:13:23 +0000

    pickup game all and single function added

--- Large assignment II - Hoop Dreams/hoop_dreams/data/schemas/pickupGameSchema.js
+++ Large assignment II - Hoop Dreams/hoop_dreams/data/schemas/pickupGameSchema.js
@@ -1,6 +1,7 @@
 const Schema = require('mongoose').Schema
 
 module.exports = new Schema({
+  _id: { type: String, required: true },
   start: { type: Date, required: true },
   end: { type: Date, required: true },
   locationId: { type: String, required: true },
--- Large assignment II - Hoop Dreams/hoop_dreams/resolvers/pickupGameResolver.js
+++ Large assignment II - Hoop Dreams/hoop_dreams/resolvers/pickupGameResolver.js
@@ -1,8 +1,28 @@
+const { ObjectId } = require('mongodb')
+
 module.exports = {
   queries: {
-    allPickupGames: () => [],
-    pickupGame: () => ({})
+    allPickupGames: (parent, args, {db}) => {
+      return db.PickupGame.find()
+        .then(pickupGames => {
+          return pickupGames
+        }).catch(err => {
+          console.error(err)
+        })
+      },
+      pickupGame: (parent, {id}, {db}) => {
+        return db.PickupGame.findOne(ObjectId(id))
+          .then(pickupGame => {
+            return pickupGame
+          }).catch(err => {
+            console.error(err)
+          })
+      }
+      
+    
+    
   },
+    
   mutations: {
     createPickupGame: () => {},
     removePickupGame: () => {},
