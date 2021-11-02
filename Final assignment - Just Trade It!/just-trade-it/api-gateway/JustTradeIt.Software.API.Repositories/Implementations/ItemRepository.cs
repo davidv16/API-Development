e51fa9c87d391ef8c28f8d530b52bc2660853263
Davíð Valdimar Arnalds <david@Davis-iMac-2.local>
2021-11-02 00:01:10 +0000

    working on trade repository

--- Final assignment - Just Trade It!/just-trade-it/api-gateway/JustTradeIt.Software.API.Repositories/Implementations/ItemRepository.cs
+++ Final assignment - Just Trade It!/just-trade-it/api-gateway/JustTradeIt.Software.API.Repositories/Implementations/ItemRepository.cs
@@ -5,6 +5,13 @@ using JustTradeIt.Software.API.Models;
 using JustTradeIt.Software.API.Repositories.Interfaces;
 using JustTradeIt.Software.API.Repositories.Contexts;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using JustTradeIt.Software.API.Models.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+   
+using System.Collections.Generic;
+//using Microsoft.AspNetCore.Authorization;
 
 namespace JustTradeIt.Software.API.Repositories.Implementations
 {
@@ -19,7 +26,21 @@ namespace JustTradeIt.Software.API.Repositories.Implementations
 
         public string AddNewItem(string email, ItemInputModel item)
         {
-            throw new NotImplementedException();
+            var nextId = _dbContext.Items.Max(table => table.Id) + 1;
+            var newIdentifier = Guid.NewGuid().ToString();
+            _dbContext.Items.Add(new Item
+            {
+                Id = nextId,
+                PublicIdentifier = newIdentifier,
+                Title = item.Title,
+                Description = item.Description,
+                ShortDescription = item.ShortDescription,
+                OwnerId = _dbContext.Users.FirstOrDefault(n => n.Email == email).Id
+            });
+
+            _dbContext.SaveChanges();
+
+            return newIdentifier;
         }
 
         public Envelope<ItemDto> GetAllItems(int pageSize, int pageNumber, bool ascendingSortOrder)
@@ -32,20 +53,58 @@ namespace JustTradeIt.Software.API.Repositories.Implementations
                     ShortDescription = n.ShortDescription,
                     Owner = new UserDto
                     {
+                        Identifier = n.Owner.PublicIdentifier,
+                        FullName = n.Owner.FullName,
+                        Email = n.Owner.Email,
+                        ProfileImageUrl = n.Owner.ProfileImageUrl
                     }
-                });
-
-            return item;
+                }).ToList();
+            //TODO: figure out how to return
+            throw new NotImplementedException();
         }
 
         public ItemDetailsDto GetItemByIdentifier(string identifier)
         {
-            throw new NotImplementedException();
+            var item = _dbContext.Items
+                .Where(n => n.PublicIdentifier == identifier)
+                .Include(i => i.ItemImages)
+                .Include(u => u.Owner)
+                .Select(n => new ItemDetailsDto
+                {
+                    Identifier = n.PublicIdentifier,
+                    Title = n.Title,
+                    Description = n.Description,
+                    Images = n.ItemImages.Select(n => new ImageDto
+                    {
+                        Id = n.Id,
+                        ImageUrl = n.ImageUrl
+
+                    }),
+                    NumberOfActiveTradeRequests = 2,
+                    Condition = "dfa",
+                    Owner = new UserDto 
+                    {
+                        Identifier = n.Owner.PublicIdentifier,
+                        FullName = n.Owner.FullName,
+                        Email = n.Owner.Email,
+                        ProfileImageUrl = n.Owner.ProfileImageUrl,
+                        //TODO: figure out how to return tokenid?
+                        //TokenId = n.Owner.
+
+                    }
+                }).FirstOrDefault();
+
+            return item;
         }
 
         public void RemoveItem(string email, string identifier)
         {
-            throw new NotImplementedException();
+            var owner = _dbContext.Items.FirstOrDefault(n => n.Owner.Email == email);
+            if(owner == null){throw new Exception("This Item doesn't have an owner with that email address");}
+
+            var item = _dbContext.Items.Single(n => n.PublicIdentifier == identifier);
+            _dbContext.Remove(item);
+            _dbContext.SaveChanges();
         }
     }
 }
--- Final assignment - Just Trade It!/just-trade-it/api-gateway/JustTradeIt.Software.API.Repositories/Implementations/TradeRepository.cs
+++ Final assignment - Just Trade It!/just-trade-it/api-gateway/JustTradeIt.Software.API.Repositories/Implementations/TradeRepository.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using JustTradeIt.Software.API.Repositories.Interfaces;
 using JustTradeIt.Software.API.Models.InputModels;
 using JustTradeIt.Software.API.Models.Dtos;
+using JustTradeIt.Software.API.Repositories.Contexts;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Sockets;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 
 namespace JustTradeIt.Software.API.Repositories.Implementations
 {
     public class TradeRepository : ITradeRepository
     {
+        private readonly JustTradeItDbContext _dbContext;
+
+        public TradeRepository(JustTradeItDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public string CreateTradeRequest(string email, TradeInputModel trade)
         {
             throw new NotImplementedException();
@@ -15,17 +26,84 @@ namespace JustTradeIt.Software.API.Repositories.Implementations
 
         public TradeDetailsDto GetTradeByIdentifier(string identifier)
         {
-            throw new NotImplementedException();
+            var trade = _dbContext.Trades
+                .Where(n => n.PublicIdentifier == identifier)
+                .Include(i => i.TradeItems)
+                .Select(n => new TradeDetailsDto
+                {
+                    Identifier = n.PublicIdentifier,
+                    //TODO: Receiving items
+                    //TODO: Offering Items
+                    Receiver = new UserDto
+                    {
+                        Identifier = n.Receiver.PublicIdentifier,
+                        FullName = n.Receiver.FullName,
+                        Email = n.Receiver.Email,
+                        ProfileImageUrl = n.Receiver.ProfileImageUrl,
+                        //TODO: TokenId
+                    },
+                    Sender = new UserDto
+                    {
+                        Identifier = n.Sender.PublicIdentifier,
+                        FullName = n.Sender.FullName,
+                        Email = n.Sender.Email,
+                        ProfileImageUrl = n.Sender.ProfileImageUrl,
+                        //TODO: TokenId
+                    },
+                    //TODO: ReceivedDate = n.RecievedDate
+                    IssuedDate = n.IssueDate,
+                    ModifiedDate = n.ModifiedDate,
+                    ModifiedBy = n.ModifiedBy,
+                    Status = n.TradeStatus
+                }).FirstOrDefault();
+            
+            return trade;
         }
 
         public IEnumerable<TradeDto> GetTradeRequests(string email, bool onlyIncludeActive)
         {
-            throw new NotImplementedException();
+            var activeTradeRequests = _dbContext.Trades
+                .Where(n => (n.Sender.Email == email) || (n.Receiver.Email == email))
+                .Where(n => n.TradeStatus != "Accepted")
+                .Where(n => n.TradeStatus == "Active")
+                .Select(n => new TradeDto
+                {
+                    Identifier = n.PublicIdentifier,
+                    IssuedDate = n.IssueDate,
+                    ModifiedDate = n.ModifiedDate,
+                    Status = n.TradeStatus
+                }).ToList();
+            
+            //Only returns active trade requests
+            if(onlyIncludeActive) {return activeTradeRequests;}
+
+            var tradeRequests = _dbContext.Trades
+                .Where(n => (n.Sender.Email == email) || (n.Receiver.Email == email))
+                .Where(n => n.TradeStatus != "Accepted")
+                .Select(n => new TradeDto
+                {
+                    Identifier = n.PublicIdentifier,
+                    IssuedDate = n.IssueDate,
+                    ModifiedDate = n.ModifiedDate,
+                    Status = n.TradeStatus
+                }).ToList();
+            return tradeRequests;
         }
 
         public IEnumerable<TradeDto> GetTrades(string email)
         {
-            throw new NotImplementedException();
+            var acceptedTrades = _dbContext.Trades
+                .Where(n => (n.Sender.Email == email) || (n.Receiver.Email == email))
+                .Where(n => n.TradeStatus == "Accepted")
+                .Select(n => new TradeDto
+                {
+                    Identifier = n.PublicIdentifier,
+                    IssuedDate = n.IssueDate,
+                    ModifiedDate = n.ModifiedDate,
+                    Status = n.TradeStatus
+                }).ToList();
+            
+            return acceptedTrades;
         }
 
         public IEnumerable<TradeDto> GetUserTrades(string userIdentifier)
