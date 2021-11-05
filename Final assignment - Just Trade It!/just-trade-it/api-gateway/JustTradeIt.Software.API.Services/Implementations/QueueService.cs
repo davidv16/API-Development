17ffe4da0df4dfda20dc7b03a5f85d0596d0ca65
Davíð Valdimar Arnalds <david@Davis-iMac-2.local>
2021-11-05 21:40:34 +0000

    queue service implemented

--- Final assignment - Just Trade It!/just-trade-it/api-gateway/JustTradeIt.Software.API.Services/Implementations/QueueService.cs
+++ Final assignment - Just Trade It!/just-trade-it/api-gateway/JustTradeIt.Software.API.Services/Implementations/QueueService.cs
@@ -1,21 +1,51 @@
 using System;
+using System.Security.Authentication;
 using JustTradeIt.Software.API.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace JustTradeIt.Software.API.Services.Implementations
 {
     public class QueueService : IQueueService, IDisposable
     {
-        //TODO: implement
-        public void Dispose()
+        private IModel _channel;
+        private IConfiguration _configuration;
+        private IConnection _connection;
+
+        public QueueService(IConfiguration configuration)
         {
-            // TODO: Dispose the connection and channel
-            throw new NotImplementedException();
+            _configuration = configuration;
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "user",
+                Password = "pass"
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
         }
 
-        //TODO: implement
+
+        public void Dispose()
+        {
+            _connection.Close();
+            _channel.Close();
+        }
+
         public void PublishMessage(string routingKey, object body)
         {
-            throw new NotImplementedException();
+            var messageBrokerSection = _configuration.GetSection("MessageBroker");
+            _channel.BasicPublish(
+                exchange: messageBrokerSection.GetSection("ExchangeName").Value,
+                routingKey,
+                mandatory: true,
+                basicProperties: null,
+                body: ConvertJsonToBytes(body)
+            );
         }
+        private byte[] ConvertJsonToBytes(object obj) => Encoding.UTF8.GetBytes(JsonSerializer.Serialize(obj));
     }
 }
