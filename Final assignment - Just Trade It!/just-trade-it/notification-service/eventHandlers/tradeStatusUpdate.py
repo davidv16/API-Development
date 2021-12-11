d549ca4766ce451add489234fc411d33fcce6fa1
davidv16 <dabbithekking@gmail.com>
2021-12-11 00:53:54 +0000

    event handlers complete

--- Final assignment - Just Trade It!/just-trade-it/notification-service/eventHandlers/tradeStatusUpdate.py
+++ Final assignment - Just Trade It!/just-trade-it/notification-service/eventHandlers/tradeStatusUpdate.py
@@ -1,2 +1,16 @@
+from services import emailService
+
+routing_key = 'trade-update-request'
+email_queue_name = 'trade-update-queue'
+
 def setup_handler(channel, exchange_name):
-    # TODO: Implement
+    # Declare the exchange, if it doesn't exist
+    channel.exchange_declare(exchange=exchange_name, exchange_type='direct', durable=True)
+    # Declare the queue, if it doesn't exist
+    channel.queue_declare(queue=email_queue_name, durable=True)
+    # Bind the queue to a specific exchange with a routing key
+    channel.queue_bind(exchange=exchange_name, queue=email_queue_name, routing_key=routing_key)
+
+    channel.basic_consume(email_queue_name,
+                      emailService.send_update_trade_email,
+                      auto_ack=False)
