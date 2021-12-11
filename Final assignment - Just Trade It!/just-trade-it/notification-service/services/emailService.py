34291e6e26155cd78099ce13802bc638ef955978
davidv16 <dabbithekking@gmail.com>
2021-12-11 00:52:38 +0000

    emailService implemented

--- Final assignment - Just Trade It!/just-trade-it/notification-service/services/emailService.py
+++ Final assignment - Just Trade It!/just-trade-it/notification-service/services/emailService.py
@@ -1,2 +1,67 @@
+import requests
+import json
+
+email_template = '''
+    <h2>Thank you for trading @ Just Trade It!</h2>
+    %s
+    <p>We hope you enjoyed our lovely service and don\'t hesitate to contact us if there are any questions.</p>
+'''
+
+new_trade_email_template = '''
+    <p>You have a new trade request!</p>
+'''
+
+update_trade_email_template = '''
+    <h3>Receiving Items:</h3>
+    <table>
+        <thead><tr style="background-color: rgba(155, 155, 155, .2)"><th>Title</th><th>Description</th></tr></thead>
+        <tbody>%s</tbody>
+    </table>
+    <h3>Offering Items:</h3>
+    <table>
+        <thead><tr style="background-color: rgba(155, 155, 155, .2)"><th>Title</th><th>Description</th></tr></thead>
+        <tbody>%s</tbody>
+    </table>
+    <p>
+        From: %s
+        IssuedDate: %s
+        ModifiedDate: %s
+        Status: %s
+    </p>
+'''
+
 def send_simple_message(to, subject, body):
-    # TODO: Implement
+    print(to, subject, body)
+
+    return requests.post(
+        "https://api.mailgun.net/v3/sandboxfe065e41f8b94f939a030875e366b1e7.mailgun.org/messages",
+        auth=("api", "44ee087810801c828734f4c330ab22f2-2ac825a1-aea71787"),
+        data={"from": "Mailgun Sandbox <postmaster@sandboxfe065e41f8b94f939a030875e366b1e7.mailgun.org>",
+              "to": to,
+              "subject": subject,
+              "html": body})
+
+def send_new_trade_email(ch, method, properties, data):
+    parsed_msg = json.loads(data)
+
+    representation = email_template % new_trade_email_template
+    send_simple_message(parsed_msg['Email'], 'New trade request!', representation)
+
+
+def send_update_trade_email(ch, method, properties, data):
+    parsed_msg = json.loads(data)
+
+    receiving_items = parsed_msg['ReceivingItems']
+    receiving_items_html = ''.join([ '<tr><td>%s</td><td>%s</td></tr>' % (item['Title'], item['ShortDescription']) for item in receiving_items ])
+    offering_items = parsed_msg['OfferingItems']
+    offering_items_html = ''.join([ '<tr><td>%s</td><td>%s</td></tr>' % (item['Title'], item['ShortDescription']) for item in offering_items ])
+    sender = parsed_msg['Sender']
+    representation = email_template % (update_trade_email_template % (
+        receiving_items_html,
+        offering_items_html,
+        '%s (%s)' % (sender['FullName'], sender['Email']),
+        parsed_msg['IssuedDate'],
+        parsed_msg['ModifiedDate'],
+        parsed_msg['Status']
+    ))
+    send_simple_message(parsed_msg['Email'], 'Trade request update!', representation)
